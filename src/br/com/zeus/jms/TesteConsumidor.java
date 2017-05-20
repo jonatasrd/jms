209b4fa47c3e1e0b874114ca25d7529c109cfc71
Jonatas Ramos <jonatasrd@gmail.com>
2017-05-20 16:45:11 -0300

    commit inicial

---
+++ src/br/com/zeus/jms/TesteConsumidor.java
@@ -0,0 +1,53 @@
+package br.com.zeus.jms;
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class TesteConsumidor {
+
+	public static void main(String[] args) throws NamingException, JMSException {
+
+		InitialContext context = new InitialContext();
+		ConnectionFactory cf = (ConnectionFactory) context.lookup("ConnectionFactory");
+
+		Connection connection = cf.createConnection();
+		connection.start();
+
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Destination fila = (Destination) context.lookup("financeiro");
+		MessageConsumer consumer = session.createConsumer(fila);
+
+		// Message message = consumer.receive();
+		// System.out.println("Recebendo msg: " + message);
+
+		consumer.setMessageListener(new MessageListener() {
+			@Override
+			public void onMessage(Message message) {
+				TextMessage txtMessage = (TextMessage) message;
+				try {
+					System.out.println("Recebendo msg: " + txtMessage.getText());
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		new Scanner(System.in).nextLine();
+
+		session.close();
+		connection.close();
+		context.close();
+	}
+
+}
